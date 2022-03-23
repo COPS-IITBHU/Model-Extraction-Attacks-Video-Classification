7392e43a7b1657810f57570c0bee88fadd865825
hex-plex <sendhilsom7@gmail.com>
2022-03-23 21:33:23 +0530

    added package style

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setuptools.setup(
     name="vidmodex",
     version="0.0.1",
     description="Code for Model Extration for Video Classification for Kinetics 400/600 trained model",
-    packages=setuptools.find_packages(include=".")
+    packages=["."],
 )
