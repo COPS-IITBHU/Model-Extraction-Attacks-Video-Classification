4bf152a640fc6f4b3f53f50af69e169a93fa73ce
hex-plex <sendhilsom7@gmail.com>
2022-03-23 22:55:44 +0530

    fixed relative path

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,3 @@
-from models.movinet import MoViNet
-from models.vivit import ViViT
-from models.swint import SwinTransformer3D as SwinT
+from .movinet import MoViNet
+from .vivit import ViViT
+from .swint import SwinTransformer3D as SwinT
